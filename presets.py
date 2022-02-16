6502ee7f89d1ad3a4db468da2e200cff7a6b85d6
Tung Duong <47948634+Mushroomcat9998@users.noreply.github.com>
2022-02-16 15:04:11 +0700

    Fix range of max_size for Resize

--- presets.py
+++ presets.py
@@ -5,7 +5,7 @@ import transforms as T
 class SegmentationPresetTrain:
     def __init__(self, base_size, crop_size, hflip_prob=0.5, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)):
         min_size = int(0.5 * base_size)
-        max_size = int(2.0 * base_size)
+        max_size = int(1.5 * base_size)
 
         trans = [T.RandomResize(min_size, max_size)]
         if hflip_prob > 0:
